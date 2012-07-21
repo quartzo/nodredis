a162d4c56e79690627900f475340cac351ae4cb8
Fabio Ferrari <fabioferrari@gmail.com>
2012-07-21 12:11:33 -0300

    how to use

--- test.js
+++ test.js
@@ -1,16 +1,4 @@
-nodredis
-========
-
-Node Redis client, really simple and small
-
-How to Use
-==========
-
-Connection
-----------
-```javascript
-var nodredis = require('nodredis');
-
+var nodredis = require('./nodredis');
 var cl = new nodredis.connect();
 console.log(['is it connected?', cl.connected]);
 cl.on("connect", function () {
@@ -20,19 +8,8 @@ cl.on("connect", function () {
 cl.on("end",function () {
       console.log("connection ended");
 });
-```
-
-Normal Operation
-----------------
-```javascript
 cl.cmd('setex', 'test', 120, "One Two Three");
 cl.cmd('get', 'test', console.log);
-```
-
-Binary Data
------------
-```javascript
-cl.cmdbin('set', 'test', Buffer('c0c1c2c3c400010203', 'bin'));
+cl.cmdbin('set', 'test', Buffer('c0c1c2c3c400010203','hex'));
 cl.cmdbin('get', 'test', console.log);
-```
 
